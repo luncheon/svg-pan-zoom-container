4237b07bd96780ccdc1ea1aaf48f0dbb332c3354
luncheon <takeuck+github@gmail.com>
2019-03-12 12:34:44 +0900

    feat: add `noEmitStyle` option

--- src/zoom-on-wheel.ts
+++ src/zoom-on-wheel.ts
@@ -5,10 +5,12 @@ export interface ZoomOnWheelOptions extends ZoomOptions {
   readonly zoomAmount: number
 }
 
-export function zoomOnWheel(attributeName: string, defaultOptions: ZoomOnWheelOptions) {
-  (document.head || document.body || document.documentElement)
-    .appendChild(document.createElement('style'))
-    .textContent = `[${attributeName}]{overflow:scroll}[${attributeName}]>:first-child{width:100%;height:100%;vertical-align:middle;}`
+export function zoomOnWheel(attributeName: string, defaultOptions: ZoomOnWheelOptions, initializationOptions: { noEmitStyle?: boolean } = {}) {
+  if (!initializationOptions.noEmitStyle) {
+    (document.head || document.body || document.documentElement)
+      .appendChild(document.createElement('style'))
+      .textContent = `[${attributeName}]{overflow:scroll}[${attributeName}]>:first-child{width:100%;height:100%;vertical-align:middle;}`
+  }
 
   addEventListener('wheel', event => {
     const [target, options] = findTargetAndParseOptions(event.target as Element, attributeName)
